12d711f17b702a74c56bf30d6485b721d1e1dc83
Jalin <admin@pjialin.com>
2020-01-08 10:28:19 +0800

    优化模型文件大小，去除远程下载

--- main.py
+++ main.py
@@ -1,68 +1,8 @@
-import hashlib
-import os
-import shutil
-
-import requests
-
-from config import Config, Logger
 from web import Web
 
 
 def main():
-    model_file_check();
     Web().run()
-
-
-def model_file_check():
-    try:
-        if os.path.isfile(Config.IMAGE_MODEL_FILE):
-            model_hash_verify()
-            return
-        Logger.info('正在下载模型文件...')
-        with requests.get(Config.OCR.get('image_model_url'), stream=True) as r:
-            with open(Config.IMAGE_MODEL_FILE, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-                Logger.info('下载成功\n')
-                model_hash_verify()
-    except InvalidModelException:
-        return model_file_check()
-
-
-def model_hash_verify():
-    """
-    验证模型文件是否完整
-    :return:
-    """
-    mode_md5 = Config.OCR.get('image_model_md5')
-    if not mode_md5:
-        # 跳过检测
-        return
-    md5 = file_md5(Config.IMAGE_MODEL_FILE)
-    if md5 != mode_md5:
-        os.remove(Config.IMAGE_MODEL_FILE)
-        Logger.error('模型文件校验失败，正在重新下载')
-        raise InvalidModelException()
-    return True
-
-
-def file_md5(file_path: str) -> str:
-    """
-    获取文件 md5
-    :param file_path:
-    :return:
-    """
-    with open(file_path, 'rb') as f:
-        fmd5 = hashlib.md5()
-        while True:
-            buf = f.read(4096)
-            if not buf:
-                break
-            fmd5.update(buf)
-        return fmd5.hexdigest()
-
-
-class InvalidModelException(Exception):
-    pass
 
 
 if __name__ == '__main__':
